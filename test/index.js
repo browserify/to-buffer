286c96a52cfeee14a2ba974d78071bdd667e9360
Jordan Harband <ljharb@gmail.com>
2025-06-18 14:50:59 -0700

    [Tests] add coverage
    Co-authored-by: Jordan Harband <ljharb@gmail.com>
    Co-authored-by: Nikita Skovoroda <chalkerx@gmail.com>

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@
 var test = require('tape');
 var availableTypedArrays = require('available-typed-arrays')();
 var forEach = require('for-each');
+var typedArrayBuffer = require('typed-array-buffer');
+var SafeBuffer = require('safe-buffer').Buffer;
 
 var toBuffer = require('../');
 var fixtures = require('./fixtures.json');
@@ -65,6 +67,42 @@ test('handle all TA types', function (t) {
 			new Buffer(fixtures[type].output),
 			type + ' should be converted to Buffer correctly'
 		);
+	});
+
+	t.test('TA subset view on another one', { skip: typeof Float64Array === 'undefined' || typeof Uint8Array === 'undefined' }, function (st) {
+		var arr = new Uint8Array(100);
+		for (var i = 0; i < arr.length; i += 1) {
+			arr[i] = i;
+		}
+
+		var buffer = typedArrayBuffer(arr);
+		var expectedHex = '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f60616263';
+
+		forEach(availableTypedArrays, function (type) {
+			var TA = global[type];
+			var offset = 8;
+			var len = 4;
+			var arr2 = new TA(buffer, offset, len);
+			st.ok(arr2.BYTES_PER_ELEMENT >= 1, 'BYTES_PER_ELEMENT: sanity check');
+
+			var expectedSlice = expectedHex.slice(offset * 2, (offset + (len * arr2.BYTES_PER_ELEMENT)) * 2);
+			var expected = SafeBuffer.from(expectedSlice, 'hex');
+			st.equal(expected.length, len * arr2.BYTES_PER_ELEMENT, 'expected length: sanity check');
+
+			var result = toBuffer(arr2);
+			st.deepEqual(
+				result,
+				expected,
+				'Uint8Array subset view on ' + type + ' should be converted to Buffer correctly'
+			);
+			st.equal(
+				result.toString('hex'),
+				expectedSlice,
+				'Uint8Array subset view on ' + type + ' should be converted to Buffer that toStrings correctly'
+			);
+		});
+
+		st.end();
 	});
 
 	t.end();
